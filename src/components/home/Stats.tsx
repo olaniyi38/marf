5a231c2eb36c578d09e691806dbccbba9018e67d
sodiq <sodiqalao38@gmail.com>
2024-03-31 22:26:57 +0100

    ui fix

--- src/components/home/Stats.tsx
+++ src/components/home/Stats.tsx
@@ -24,7 +24,7 @@ const Stats = () => {
 		<section className="">
 			<div className="grid md:grid-cols-3 bg-gray-200 py-8 md:px-8">
 				{stats.map(({ value, title, url }) => (
-					<div className="py-16  group even:bg-white even:text-brick-red odd:bg-mandy odd:text-white space-y-8 flex flex-col items-center max-w-[15rem] md:w-full md:max-w-none mx-auto">
+					<div className="py-16  group even:bg-white even:text-brick-red odd:bg-mandy odd:text-white space-y-8 flex flex-col items-center w-[18rem] md:max-w-[20rem] md:w-full  mx-auto">
 						<p className=" text-5xl font-black">{value}K</p>
 						<p className=" capitalize">{title}</p>
 						<a
